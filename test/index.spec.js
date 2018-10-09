f025d571544e336d46d51193ac4d82f119c39963
Eko Eryanto <ekoeryanto@gmail.com>
2018-10-09 11:38:57 +0700

    chore: update test [skip ci]

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,18 +1,30 @@
 /* global describe, it, before */
 const chai = require('chai');
-const icons = require('../');
+const list = require('../dist/list.cjs');
+const categories = require('../dist/categories.cjs');
 
 chai.expect();
 
 const expect = chai.expect;
 
 describe('icon list', () => {
-  it('is an object and not empty', () => {
-    expect(icons)
-      .to.be.an('object').that.is.not.empty;
-  });
-  it('object has array of categories and not empty', () => {
-    expect(icons.categories)
+  it('is an array and not empty', () => {
+    expect(list)
       .to.be.an('array').that.is.not.empty;
+  });
+  // it('object has array of categories and not empty', () => {
+  //   expect(icons.categories)
+  //     .to.be.an('array').that.is.not.empty;
+  // });
+});
+
+describe('icon categories', () => {
+  it('is an array and not empty', () => {
+    expect(categories)
+      .to.be.an('array').that.is.not.empty;
+  });
+  it('array contain object of icons', () => {
+    expect(categories[0])
+      .to.be.an('object').to.have.property('icons');
   });
 });
