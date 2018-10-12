0b37032514fcb693f56e4873ccf95b54bbd07232
ekoeryanto <ekoeryanto@gmail.com>
2018-10-12 08:33:22 +0700

    feat: iife for small bundle
    BEAKING CHANGE:
    pkg.browser now use iife bundle
    bundles structure dir changed to `dist/format/file.js` except for iife `dist/file.js`

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,22 @@
 import json from 'rollup-plugin-json';
+import { resolve } from 'path'
 
 const plugins = [json()];
 
 const files = ['categories', 'icons', 'ids', 'index', 'list'];
-const formats = ['umd', 'cjs', 'esm'];
+const formats = ['umd', 'cjs', 'esm', 'iife'];
 
 const createOutput = ({format, file}) => {
-  if (format === 'umd') {
-    return {
-      format,
-      file: `dist/${file}.js`,
-      name: 'window',
-      extend: true
-    };
-  }
-  return {
+  let output = {
     format,
-    file: `dist/${file}.${format}.js`
-  };
+    file: resolve('dist', format === 'iife' ? '' : format, `${file}.js`)
+  }
+
+  if (format === 'umd' || format === 'iife') {
+    output.name = 'materialDesignIconList'
+  }
+
+  return output
 };
 
 export default files.map(file => {
